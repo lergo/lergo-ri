4f328b3a791ea9e29b52dfab4ce0a2fdb6f61c23
guy.mograbi@gmail.com <guymograbi>
2014-04-13 10:37:54 +0300

    updating express to the new version.

--- server.js
+++ server.js
@@ -6,6 +6,11 @@
 var port = 3000;
 
 var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var cookieParser = require('cookie-parser');
+var cookieSession = require('cookie-session');
+var errorHandler = require('errorhandler');
 //var routes = require('./backend/Routes');
 var swagger = require('swagger-node-express');
 var passport = require('passport');
@@ -38,19 +43,11 @@ logger.info(typeof(useStatic));
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(express.cookieParser());
-app.use(express.cookieSession( { 'secret' : appContext.conf.cookieSessionSecret } ));
+app.use(bodyParser());
+app.use(methodOverride());
+app.use(cookieParser());
+app.use(cookieSession( { 'secret' : appContext.conf.cookieSessionSecret } ));
 app.use('/backend/user', controllers.users.loggedInMiddleware);
-app.use(app.router);
-app.use('/public', express.static(__dirname + '/public'));
-app.use('/swagger', function () {
-    return useStatic.apply(this, arguments);
-});
-
-
-app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 
 
 // Routes
@@ -148,6 +145,16 @@ var server = app.listen(port, function () {
 //    logger.info('possible routes are : ' +JSON.stringify(app.routes.routes,{},4));
 });
 
+
+
+app.use('/public', express.static(__dirname + '/public'));
+app.use('/swagger', function () {
+    return useStatic.apply(this, arguments);
+});
+
+
+app.use(errorHandler({ dumpExceptions: true, showStack: true }));
+
 logger.info('catching all exceptions');
 // catch the uncaught errors that weren't wrapped in a domain or try catch statement
 // do not use this in modules, but only in applications, as otherwise we could have multiple of these bound
@@ -156,4 +163,6 @@ process.on('uncaughtException', function (err) {
     // handle the error safely
     logger.info(err);
 });
+
+
 
