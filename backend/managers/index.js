31f5593231b5037245c5c0d77cd79852633a7f45
guy.mograbi@gmail.com <guymograbi>
2014-05-12 07:01:51 +0300

    fixing reference to manager.

--- backend/managers/index.js
+++ backend/managers/index.js
@@ -4,4 +4,4 @@ exports.log = require('./LogManager');
 exports.users = require('./UsersManager');
 exports.questions=require('./QuestionsManager');
 exports.lessons=require('./LessonsManager');
-exports.lessonsInvitations = require('./LessonsInvitations');
+exports.lessonsInvitations = require('./LessonsInvitationsManager');
