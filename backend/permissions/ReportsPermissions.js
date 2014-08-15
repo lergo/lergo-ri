5b45368199fa4252776fadfbb1608b694881974a
shukla2009 <shukla2009@gmail.com>
2014-08-16 05:07:04 +0530

    lergo-336 : Add "Test Mode" for playing a quiz (in addition to today's "practice mode")

--- backend/permissions/ReportsPermissions.js
+++ backend/permissions/ReportsPermissions.js
@@ -1,15 +1,16 @@
 'use strict';
 
-
-
-exports.userCanDelete = function( report ,user ){
-    return !!user.isAdmin || report.data.inviter.equals(user._id);
+exports.userCanDelete = function(report, user) {
+	return !!user.isAdmin || report.data.inviter===user._id.toString();
 };
 
+exports.userCanDeleteItSelf = function(report, user) {
+	return report.userId.equals(user._id);
+};
 
-
-exports.getPermissions = function( report, user ){
-    return {
-        'canDelete' : exports.userCanDelete(report,user)
-    };
+exports.getPermissions = function(report, user) {
+	return {
+		'canDelete' : exports.userCanDelete(report, user),
+		'canDeleteItSelf' : exports.userCanDeleteItSelf(lesson, user)
+	};
 };
