6148702ea816e96f246ba7a08980527e6411f369
guy.mograbi@gmail.com <guymograbi>
2014-08-18 00:34:13 +0300

    adding function to mock.

--- backend/services/disqus/mock.js
+++ backend/services/disqus/mock.js
@@ -4,3 +4,7 @@ var logger = require('log4js').getLogger('disqusMock');
 exports.configure = function(/*conf*/){
     logger.info('configuring');
 };
+
+exports.ssoObj = function(){
+    logger.info('mock returns sso obj');
+};
