6cd7ec533154ed8f8d3be8b39df92f369864b5e2
guy.mograbi@gmail.com <guymograbi>
2014-09-12 15:19:20 +0300

    LERGO-420 - fixing service script.

--- status.sh
+++ status.sh
@@ -2,8 +2,8 @@ PIDFILE=lergo.pid
 PROCFILE=/proc/`cat $PIDFILE`
 if [ -e $PROCFILE ]; then
     echo "service is running"
-    return 0
+    exit 0
 else
     echo "service is stopped"
-    return 0
+    exit 0
 fi
