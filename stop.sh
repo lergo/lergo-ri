4c291ee7e901d375c6609fa9f8daf020dfc9b43b
guy.mograbi@gmail.com <guymograbi>
2014-09-30 00:42:43 +0300

    LERGO-420 fixing stop script

--- stop.sh
+++ stop.sh
@@ -4,7 +4,7 @@ echo `pwd`
 
 SYSCONFIG=/etc/sysconfig/lergo
 source $SYSCONFIG &> /dev/null
-
+      /var/run/lergo.pid
 
 PIDFILE=lergo.pid
 PIDNUMBER=`cat /var/run/$PIDFILE`
@@ -14,13 +14,13 @@ if [ ! -e $PROCFILE ] || ! kill -0 $PIDNUMBER; then
     exit 1
 fi
 
-if [ "$USE_FORVER" = "true" ];then
+if [ "$USE_FOREVER" = "true" ];then
     echo "stopping forever" >&2
     forever stop server.js
     echo "forever stopped" >&2
 else
     echo 'Stopping service...' >&2
-    kill -15 $(cat "$PIDFILE") && rm -f "$PIDFILE"
+    kill -15 $(cat "/var/run/$PIDFILE") && rm -f "/var/run/$PIDFILE"
     echo 'Service stopped' >&2
 fi
 
