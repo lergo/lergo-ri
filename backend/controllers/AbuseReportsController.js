761783c6daa726861970c4ffb405b8d57fee7d62
shukla2009 <shukla2009@gmail.com>
2014-11-04 15:44:01 +0530

    lergo-550-final

--- backend/controllers/AbuseReportsController.js
+++ backend/controllers/AbuseReportsController.js
@@ -1,121 +1,24 @@
 'use strict';
-var models = require('../models');
+var AbuseReport = require('../models/AbuseReport');
 var managers = require('../managers');
 var logger = require('log4js').getLogger('AbuseReportsController');
-var services = require('../services');
-var async = require('async');
-var _ = require('lodash');
-
-// enum temporary solution for conversion
-var localization = {
-	notAppropriate : 'it is not appropriate for kids',
-	notEducational : 'it is not educational content',
-	infringesRights : 'it infringes on my rights'
-};
-
-function sendMail(emailVars, callback) {
-	async.parallel([ function(done) {
-		services.emailTemplates.renderAbuseReportEmail(emailVars, function(err, html, text) {
-			services.email.sendMail({
-				to : emailVars.creatorEmail,
-				subject : 'Flagged ' + emailVars.itemType + ' report re "' + emailVars.title + '"',
-				text : text,
-				html : html
-			}, function(err) {
-				if (!!err) {
-					logger.error('error while sending report to craetor', err);
-					done(err);
-				}
-				done();
-			});
-		});
-	}, function(done) {
-		services.emailTemplates.renderAbuseReportAdminEmail(emailVars, function(err, htmlAdmin, textAdmin) {
-			services.email.sendMail({
-				to : 'rahul.shukla@synerzip.com',
-				subject : 'Flagged ' + emailVars.itemType + ' report re "' + emailVars.title + '"',
-				text : textAdmin,
-				html : htmlAdmin
-			}, function(err) {
-				if (!!err) {
-					logger.error('error while sending report to admin', err);
-					done(err);
-				}
-				done();
-			});
-		});
-	} ], function(err) {
-		callback(err);
-	});
-
-}
-
-function sendAbuseReportAlert(emailResources, report, callback) {
-	logger.info('send abuseReport alert  is ready email');
-	async.parallel([ function(done) {
-		report.getCreator(function(err, result) {
-			if (!!err) {
-				done(err);
-			}
-			done(null, result);
-		});
-	}, function(done) {
-		report.getReporter(function(err, result) {
-			if (!!err) {
-				done(err);
-			}
-			done(null, result);
-		});
-	} ], function(err, results) {
-		if (!!err) {
-			callback(err);
-			return;
-		}
-		var creator = results[0];
-		var reporter = results[1];
-		var emailVars = {};
-		_.merge(emailVars, emailResources);
-		_.merge(emailVars, {
-			creatorName : creator.username,
-			creatorEmail : creator.email,
-			reporterName : reporter.username,
-			title : report.data.title,
-			itemType : report.data.itemType,
-			reason : localization[report.data.reason],
-			comment : report.data.comment
-		});
-		sendMail(emailVars, function(err) {
-			if (!!err) {
-				callback(err);
-				return;
-			}
-			logger.info('saving report sent true');
-			report.setSent(true);
-			report.update();
-			callback();
-		});
-
-	});
-}
 
 exports.abuse = function(req, res) {
-
-	var abuseReport = models.AbuseReport.createNewFromRequest(req);
+	var abuseReport = AbuseReport.createNewFromRequest(req);
 	if (!!abuseReport._id) {
 		res.status(500).send({
 			'message' : 'abuse already reported'
 		});
 		return;
 	}
-
-	models.AbuseReport.connect(function(db, collection) {
+	AbuseReport.connect(function(db, collection) {
 		collection.insert(abuseReport, function(err, result) {
 			if (!!err || !result) {
 				logger.error('unable to insert abusreReport to db', err);
 				new managers.error.InternalServerError(err, 'unable to report abuse').send(res);
 				return;
 			}
-			sendAbuseReportAlert(req.emailResources, new models.AbuseReport(abuseReport), function(err) {
+			managers.abuseReports.sendAbuseReportAlert(req.emailResources, new AbuseReport(abuseReport), function(err) {
 				if (!!err) {
 
 					new managers.error.InternalServerError(err, 'unable to report abuse').send(res);
@@ -128,7 +31,6 @@ exports.abuse = function(req, res) {
 };
 
 exports.getAll = function(req, res) {
-
 	managers.abuseReports.complexSearch(req.queryObj, function(err, result) {
 		if (!!err) {
 			new managers.error.InternalServerError(err, 'unable to get all admin lessons').send(res);
@@ -140,7 +42,7 @@ exports.getAll = function(req, res) {
 
 exports.deleteReport = function(req, res) {
 	logger.debug('deleting abuseReport');
-	models.AbuseReport.connect(function(db, collection) {
+	AbuseReport.connect(function(db, collection) {
 		logger.debug('removing abuseReport');
 		collection.remove(req.abuseReport, function(err, result) {
 			logger.debug('after remove abuseReport, err and result are', err, result);
@@ -156,7 +58,7 @@ exports.deleteReport = function(req, res) {
 exports.update = function(req, res) {
 	logger.info('updating abuseReport');
 	var abuseReport = req.body;
-	new models.AbuseReport(abuseReport).update(function(err) {
+	new AbuseReport(abuseReport).update(function(err) {
 		logger.info('abuseReport updated');
 		if (!!err) {
 			new managers.error.InternalServerError(err, 'unable to update abuseReport').send(res);
--- backend/managers/AbuseReportsManager.js
+++ backend/managers/AbuseReportsManager.js
@@ -7,6 +7,9 @@
 
 var services = require('../services');
 var AbuseReport = require('../models/AbuseReport');
+var async = require('async');
+var logger = require('log4js').getLogger('AbuseReportsManager');
+var _ = require('lodash');
 
 exports.complexSearch = function(queryObj, callback) {
 
@@ -34,5 +37,97 @@ exports.complexSearch = function(queryObj, callback) {
 		services.complexSearch.complexSearch(queryObj, {
 			collection : collection
 		}, callback);
+	});
+};
+
+// enum temporary solution for conversion
+var localization = {
+	notAppropriate : 'it is not appropriate for kids',
+	notEducational : 'it is not educational content',
+	infringesRights : 'it infringes on my rights'
+};
+
+function sendMail(emailVars, callback) {
+	async.parallel([ function(done) {
+		services.emailTemplates.renderAbuseReportEmail(emailVars, function(err, html, text) {
+			services.email.sendMail({
+				to : emailVars.creatorEmail,
+				subject : 'Flagged ' + emailVars.itemType + ' report re "' + emailVars.title + '"',
+				text : text,
+				html : html
+			}, function(err) {
+				if (!!err) {
+					logger.error('error while sending report to craetor', err);
+					done(err);
+				}
+				done();
+			});
+		});
+	}, function(done) {
+		services.emailTemplates.renderAbuseReportAdminEmail(emailVars, function(err, htmlAdmin, textAdmin) {
+			services.email.sendMail({
+				to : services.conf.adminEmail,
+				subject : 'Flagged ' + emailVars.itemType + ' report re "' + emailVars.title + '"',
+				text : textAdmin,
+				html : htmlAdmin
+			}, function(err) {
+				if (!!err) {
+					logger.error('error while sending report to admin', err);
+					done(err);
+				}
+				done();
+			});
+		});
+	} ], function(err) {
+		callback(err);
+	});
+
+}
+
+exports.sendAbuseReportAlert = function(emailResources, report, callback) {
+	logger.info('send abuseReport alert  is ready email');
+	async.parallel([ function(done) {
+		report.getCreator(function(err, result) {
+			if (!!err) {
+				done(err);
+			}
+			done(null, result);
+		});
+	}, function(done) {
+		report.getReporter(function(err, result) {
+			if (!!err) {
+				done(err);
+			}
+			done(null, result);
+		});
+	} ], function(err, results) {
+		if (!!err) {
+			callback(err);
+			return;
+		}
+		var creator = results[0];
+		var reporter = results[1];
+		var emailVars = {};
+		_.merge(emailVars, emailResources);
+		_.merge(emailVars, {
+			creatorName : creator.username,
+			creatorEmail : creator.email,
+			reporterName : reporter.username,
+			title : report.data.title,
+			itemType : report.data.itemType,
+			reason : localization[report.data.reason],
+			comment : report.data.comment
+		});
+		sendMail(emailVars, function(err) {
+			if (!!err) {
+				callback(err);
+				return;
+			}
+			logger.info('saving report sent true');
+			report.setSent(true);
+			report.update();
+			callback();
+		});
+
 	});
 };
