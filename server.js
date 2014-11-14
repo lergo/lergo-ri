520e8872f4f208eed8d7cd58f4bf2012b03d0193
guy.mograbi@gmail.com <guymograbi>
2014-11-14 16:41:17 +0200

    fix build.

--- server.js
+++ server.js
@@ -6,6 +6,7 @@
 var port = 3000;
 
 var express = require('express');
+var _ = require('lodash');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var cookieParser = require('cookie-parser');
