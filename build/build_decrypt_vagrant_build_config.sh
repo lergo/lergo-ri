eff8cc8087f52f61598eb63dc842a5f1872c0808
guy.mograbi@gmail.com <guymograbi>
2014-11-23 22:02:29 +0200

    task(build): make build run on vagrant

--- build/build_decrypt_vagrant_build_config.sh
+++ build/build_decrypt_vagrant_build_config.sh
@@ -2,6 +2,7 @@
 
 # use this script during jenkins.sh build
 # this should hide secret environment variables
+# run this script from lergo-ri root folder
 
 
-openssl aes-256-cbc -d -a -in lergo-ri/build/vagrant-build.json.enc -out $VAGRANT_BUILD_CONF -pass pass:$TESTME_KEY
+openssl aes-256-cbc -d -a -in build/vagrant-build.json.enc -out $VAGRANT_BUILD_CONF -pass pass:$TESTME_KEY
