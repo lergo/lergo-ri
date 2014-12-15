5b95d3c953bf7f1b5c11e13b6c2557affcd705f3
shukla2009 <shukla2009@gmail.com>
2014-12-15 20:23:06 +0530

    lergo-570

--- backend/controllers/LessonsController.js
+++ backend/controllers/LessonsController.js
@@ -28,12 +28,16 @@ exports.getUserLikedLessons = function(req, res) {
 		itemType : 'lesson'
 	}, {
 		itemId : 1,
-		_id : 0
+		_id : 1
 	}, function(err, result) {
 		if (!!err) {
 			err.send(res);
 			return;
 		}
+		var mapResults = {};
+		_.each(result, function(r) {
+			mapResults[r.itemId.toString()] = r._id.getTimestamp();
+		});
 		queryObj.filter._id = {
 			$in : _.map(result, 'itemId')
 		};
@@ -42,8 +46,10 @@ exports.getUserLikedLessons = function(req, res) {
 				err.send(res);
 				return;
 			} else {
-				res.send(obj);
-				return;
+				_.each(obj.data, function(o) {
+					o.lastUpdate = mapResults[o._id.toString()];
+				});
+				return res.send(obj);
 			}
 		});
 	});
