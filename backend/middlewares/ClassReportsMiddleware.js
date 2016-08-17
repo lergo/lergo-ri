91533342d42ea0bec5ed57d795a7015124a970b5
Rahul Shukla <shukla2009@gmail.com>
2016-08-18 01:41:42 +0530

    class report aggregation should be done once any report is modified

---
+++ backend/middlewares/ClassReportsMiddleware.js
@@ -0,0 +1,27 @@
+'use strict';
+var Report = require('../models').ClassReport;
+var logger = require('log4js').getLogger('ReportsMiddleware');
+
+exports.exists = function exists(req, res, next) {
+	logger.debug('checking if report exists : ', req.params.reportId);
+	try {
+		Report.findById(req.params.reportId, function(err, result) {
+			if (!!err) {
+				res.send(500, err);
+				return;
+			}
+			if (!result) {
+				res.send(404);
+				return;
+			}
+
+			logger.debug('putting report on request', result);
+			req.report = result;
+
+			next();
+
+		});
+	} catch (e) {
+		res.send(404);
+	}
+};
