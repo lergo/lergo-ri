86249328b33dae6247af1c7c4fb768bf332e53f4
jlevym <jlevym@gmail.com>
2016-09-02 16:22:01 +0300

    fix: add key for error message 'user not validated' : login.userNotValidated

--- backend/managers/ErrorManager.js
+++ backend/managers/ErrorManager.js
@@ -34,7 +34,7 @@ var errorsDefinition = {
     'NotAdmin' : createNewError(4, 'requires admin permissions', 401),
     'WrongLogin' : createNewError(5, 'wrong login', 401),
     'InvalidEmail': createNewError(6, 'invalid email', 500),
-    'UserNotValidated' : createNewError(7, 'user not validated', 500),
+    'UserNotValidated' : createNewError(7, 'login.userNotValidated', 500),
     'UserValidationError' : createNewError( 8, 'unable to validate user', 500),
     'UserValidationFailed' : createNewError(9, 'user validation failed', 401),
     'UserAlreadyValidated' : createNewError(10, 'user already validated', 500),
