e93f9219372db965b20c3e2598ca1e7fafe88491
jlevym <jlevym@gmail.com>
2016-12-26 20:07:35 +0200

    update phantom code to be compatible wit phantom npm website
    passes localhost system tests

--- server.js
+++ server.js
@@ -267,38 +267,37 @@ app.get('/backend/crawler', function(req, res){
     var url = req.param('_escaped_fragment_');
     url = req.absoluteUrl('/index.html#!' + decodeURIComponent(url) );
     logger.info('prerendering url : ' + url ) ;
-
-
+    var sitepage = null;
+    var phInstance = null;
     var phantom = require('phantom');
-    phantom.create([]).then(function (ph) {
-
-        setTimeout( function(){
-            try{
-                ph.exit();
-            }catch(e){
-                logger.debug('unable to close phantom',e);
-            }
-        }, 30000);
-
-
-        return ph.createPage(function (page) {
-            page.open(url).then(function ( status ) {
-                if ( status === 'fail'){
-                    res.send(500,'unable to open url');
-                    ph.exit();
-                }else {
-                    page.evaluate(function () {
-                        return document.documentElement.innerHTML;
-                    }, function (result) {
-                        res.send( result);
-                        ph.exit();
-                        page.close();
-                    });
-                }
-
+    phantom.create() //phantom.create
+        .then(instance => {
+        phInstance = instance;  //ph ==> phInstance
+    return instance.createPage(); // createpage
+        })
+    .then(page => {
+        // need to use page.open
+        page.open(url).then(function( status ){ //page.open
+        if ( status === 'fail'){
+            res.send(500,'unable to open url');
+            phInstance.exit();
+        }else {
+            page.evaluate(function () { //page.evaluate
+                return document.documentElement.innerHTML;
+            }).then(function (result) {
+                console.log(result);
+                res.send( result);
+                phInstance.exit();
+                page.close();
             });
-        });
+        }
+
     });
+})
+    .catch(error => {
+        console.log(error);
+    phInstance.exit();
+});
 });
 
 
