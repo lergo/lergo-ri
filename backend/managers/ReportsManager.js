b8b18b34e28f5023b52789db0e8b62c6eeb00fb8
jlevym <jlevym@gmail.com>
2018-08-01 17:16:24 +0300

    updated hebrew for markup and fixed jshint

--- backend/managers/ReportsManager.js
+++ backend/managers/ReportsManager.js
@@ -39,47 +39,42 @@ exports.sendReportLinkForClass = function (emailResources, report, callback) {
             callback(err);
             return;
         }
-        var markup = ``;
         var emailVars = {};
         _.merge(emailVars, emailResources);
         var lessonInviteLink = emailResources.lergoBaseUrl + '/#!/public/lessons/reports/agg/' + report.data.classreportId + '/display';
 
         _.merge(emailVars, { 'link': lessonInviteLink, 'name': inviter.username, 'className': report.data.className, 'inviteeName': report.getName(),'lessonTitle': report.data.data.lesson.name, 'lessonLanguage':report.data.data.lesson.language });
 
-        services.emailTemplates.renderReportReady(emailVars, function (err, html, text) {
-            var subject = 'Here is a link to your class report';
-            if (emailVars.lessonLanguage) {
-                if (emailVars.lessonLanguage === 'hebrew') {
-                    subject = 'הנה קישור לדוח הכיתה שלך';
-                } else {
-                    subject = 'Here is a link to your class report';
-                }
-            }
-            var markup = classReportMarkup();
-            services.email.sendMail({
-                'to': inviter.email,
-                'subject': subject,
-                'text': text,
-                'html': markup
-            }, function (err) {
-                if (!!err) {
-                    logger.error('error while sending report', err);
-                    callback(err);
-                } else {
-                    logger.info('saving report sent true');
-                    report.setSent(true);
-                    report.update();
-                    callback();
-                }
-            });
-        });
 
         const person = {
             imageLink: 'https://images.freeimages.com/images/large-previews/322/indian-heads-1391201.jpg'
         };
-
         function classReportMarkup() {
             if(emailVars.lessonLanguage === 'hebrew') {
+                return `
+            <img src=${person.imageLink} style="height: 100px; width: 200px; padding: 20px; border: 2px solid black">
+
+                    <p>
+                     ${emailVars.name} שלום 
+            </p>
+                <p>
+                    הדו"ח מוכן עכשיו
+                 <b>${emailVars.className}</b>
+                לחץ על הקישור להלן כדי לראות את הדוח report.The יעודכנו כמו תלמידים נוספים לסיים את השיעור
+                זהו הדוא"ל היחיד שתקבל בנוגע לשיעור או לדו"ח המחלקה.
+            <p>
+                <a href=${emailVars.link}>${emailVars.lessonTitle}</a>
+                </p>
+                <p>
+                Powered by <a href="http://www.lergo.org/">LerGO - Educate Yourself.</a><br/>
+                <a href="https://docs.google.com/forms/d/1wyO39CkCTiNAP1BrJLti0GjA8eMq6rv-QcZu0Lw1-tE/viewform?usp=send_form">Terms of Service</a> -
+                <a href="https://docs.google.com/forms/d/1YQnB8KoouFW2cJHsO_bkdxAD2ekuLYJyvaH6AuIU-Ig/viewform?usp=send_form"> Send Feedback</a> -
+                <a href="https://docs.google.com/forms/d/1t_CtQxTMJlTlLRx-3lSPv8bNfw3nVXpE2B2hGkdeSMg/viewform">Report Abuse</a> -
+                <a href="https://docs.google.com/forms/d/1rU6sTKskoTqrd9u1wgiX_zG0QpfWsxizxe8CdDRj4fk/viewform?usp=send_form">Suggest a Lesson</a> -
+                <a href="https://docs.google.com/forms/d/e/1FAIpQLSeFtpXck4viIM7aTo7GoZST3z2Cojjn0BNsb-o2IAz627fYsw/viewform?usp=sf_link">Why am I getting this email?</a><br>
+                </p>
+                    `;
+            }else{
                 return `
         <img src=${person.imageLink} style="height: 100px; width: 200px; padding: 20px; border: 2px solid black">
 
@@ -103,11 +98,40 @@ exports.sendReportLinkForClass = function (emailResources, report, callback) {
             <a href="https://docs.google.com/forms/d/e/1FAIpQLSeFtpXck4viIM7aTo7GoZST3z2Cojjn0BNsb-o2IAz627fYsw/viewform?usp=sf_link">Why am I getting this email?</a><br>
         </p>
 `;
-            }else{
-                return 'this is english'
             }
 
-        };
+        }
+
+
+        services.emailTemplates.renderReportReady(emailVars, function (err, html, text) {
+            var markup = classReportMarkup();
+            var subject = 'Here is a link to your class report';
+            if (emailVars.lessonLanguage) {
+                if (emailVars.lessonLanguage === 'hebrew') {
+                    subject = 'הנה קישור לדוח הכיתה שלך';
+                } else {
+                    subject = 'Here is a link to your class report';
+                }
+            }
+            services.email.sendMail({
+                'to': inviter.email,
+                'subject': subject,
+                'text': text,
+                'html': markup
+            }, function (err) {
+                if (!!err) {
+                    logger.error('error while sending report', err);
+                    callback(err);
+                } else {
+                    logger.info('saving report sent true');
+                    report.setSent(true);
+                    report.update();
+                    callback();
+                }
+            });
+        });
+
+
     });
 
 };
