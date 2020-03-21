cdee132d8fc521b64159f0ec2f06d7489034adf3
Jeffrey <4lergo@gmail.com>
2020-03-21 17:10:12 +0200

    implementing caching on tags for homepage to deduce load on mongodb

--- backend/controllers/TagsController.js
+++ backend/controllers/TagsController.js
@@ -30,6 +30,15 @@ exports.getTopTags = function (req, res) {
 
 //  db.lessons.aggregate( { $unwind : '$tags' },  { $group : {_id : '$tags.label' } }, { $match : { '_id' : /tom/i } } )
 //  db.lessons.aggregate( { $unwind : '$tags' },  { $match : {'tags.label' : /tom/i } }, { $group : { _id : '$tags.label' } })
+
+//  implementing caching of home page - TagsController and ComplexSearchService. Using 'Date' the variables will be reset every day
+//  the TagsController does not require different code for the different languages. 
+    // variables for caching home page tags
+    var cachedResult = [];
+    var validForCachingTags = false;
+    var previousDate = 0;
+    var currentDate = 0;
+
 exports.getTagsByFilter = function (req, res) {
 
     var like = req.param('like');
@@ -45,8 +54,16 @@ exports.getTagsByFilter = function (req, res) {
 
     var result = [];
 
+    // test requests matching homepage     
+    var strLike = '"' + like + '"';
+    var likeRequest = strLike.indexOf('(?:)') === 2;
+    var idsRequest = (lessonsId.length + questionsId.length) === 0; 
+    var d = new Date();
+    currentDate = d.getDate();
+    validForCachingTags = likeRequest && idsRequest;
 
     function findTagsOnCollection( collectionName, like, ids, callback ){
+    
         var match = { $match : { 'tags.label' : like || '' }};
         if ( !!ids && ids.length > 0){
             match.$match._id = { '$in' : ids };
@@ -67,8 +84,7 @@ exports.getTagsByFilter = function (req, res) {
         });
     }
 
-
-    function findCallback(err, cursor, next) {
+    function findCallback(err, cursor, next) {   
         cursor.toArray(function( err, tags) {
             if (!!err) {
                 next(err);
@@ -76,12 +92,9 @@ exports.getTagsByFilter = function (req, res) {
             } else {
                 result = result.concat(tags);
                 next();
-    
             }
         });
     }
-    
-
 
     function findTagsOnLessons( next ){
         findTagsOnCollection( 'lessons', like, lessonsId, function(err, tags){ findCallback( err, tags, next ); });
@@ -92,16 +105,34 @@ exports.getTagsByFilter = function (req, res) {
     }
 
     function main() {
-        async.parallel(
-            [
-                findTagsOnLessons,
-                findTagsOnQuestions
-            ],
-            function () {
-                logger.debug('finished fetching labels');
-                res.send(_.uniqBy(result, 'label'));
-            }
-        );
+        // check if request is for home page tags
+        if (validForCachingTags && cachedResult.length !== 0) {
+            logger.info('using cached tags for homepage');
+            var finalResult = cachedResult;
+            if (currentDate !== previousDate) { // reset the home page link every day
+                previousDate = currentDate;
+                logger.info('updating date to ', previousDate);
+                cachedResult = [];
+            }    
+            res.send(finalResult);
+        } else {
+            async.parallel(
+                [
+                    findTagsOnLessons,
+                    findTagsOnQuestions
+                ],
+                function () {
+                    logger.debug('finished fetching labels');
+                    var finalResult = _.uniqBy(result, 'label');
+                    if (validForCachingTags && cachedResult.length === 0) {
+                        logger.info('caching homepage tags');
+                        cachedResult = finalResult;
+                    }
+                    finalResult = finalResult;
+                    res.send(finalResult);
+                }
+            );
+        }
     }
 
     if ( lessonsId.length > 0 ){
