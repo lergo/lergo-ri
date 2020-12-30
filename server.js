953b2fd67d246bf9c926d02aec87de675d690132
Jeffrey <4lergo@gmail.com>
2020-12-30 06:39:55 +0200

    lowering express-throttle to burts: 5 rate 3/m to prevent realstaging from crashing

--- server.js
+++ server.js
@@ -302,7 +302,7 @@ app.get('/backend/sitemap.xml', function(req, res){
  var numRepeats = 0;
  var throttle = require('express-throttle');
 
- app.get('/backend/crawler', throttle({ 'burst': 15, 'rate': '10/m' }),function(req, res){
+ app.get('/backend/crawler', throttle({ 'burst': 5, 'rate': '3/m' }),function(req, res){
     var url = req.param('_escaped_fragment_');
     url = req.absoluteUrl('/index.html#!' + decodeURIComponent(url) );
 
